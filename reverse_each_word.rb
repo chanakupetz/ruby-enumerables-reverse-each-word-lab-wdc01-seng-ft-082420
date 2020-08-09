35914ffcb335687745a1e3e8c09a7ecaaa60f998
Chana Kupetz <chanakupetz@gmail.com>
2020-08-09 02:40:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,11 +9,4 @@ def reverse_each_word (string)
     return_array.join(" ")
   end
 
-  def reverse_each_word_using_collect(string)
-      array = string.split(" ") #turn string into an array
-    test_array = []
-    array.collect do|string| #iterate over the array
-      test_array << string.reverse #reverse each word in the array
-    end
-    test_array.join(" ")
-  end
+  
